faf0f33e4314f7acfc294fc07ca562d57c196e68
hengxv <1790012356@qq.com>
2021-05-29 15:33:29 +0800

    *

--- min_cost_tree/main.cpp
+++ min_cost_tree/main.cpp
@@ -11,9 +11,22 @@ typedef struct each_point                                                       
     int point;                                                                        //点的编号
     int group;                                                                        //点的组别
 }each_point;                                                                           
-void search(relation *a,each_point *b);                                               //寻找一次符合边
+struct node
+{
+    int firnode;
+    struct acrNode *next;
+};
+typedef struct acrNode                                                                //邻接表单元，用于储存最终结果
+{
+    int point;
+    int power;
+    struct acrNode *next;
+}*acrP;                                                                                   
+int search(relation *a,each_point *b);                                               //寻找一次符合边
+void save(relation a);                                                                //对已经选择的边，利用邻接表储存
 int edge,num;                                                                         //edge为边数目，num为顶点数目
 int times = 1;                                                                        //组别编号
+node *list;
 int main()
 {
     printf("请分别输入图的 边数 和 顶点 个数：\n");
@@ -21,21 +34,25 @@ int main()
     printf("请分别输入各个边的 两个顶点 及其 权重 ：\n");
     relation edge_rel[edge];                                                          //边关系
     each_point one[num+1];                                                            //顶点关系
-    for (int i = 0; i < edge;edge_rel[i].flag = 0,i++)
+    list = (struct node*)malloc(sizeof(struct node)*num);
+    for (int i = 0; i < edge;edge_rel[i++].flag = 0)                                //根据输入，构建边的特征
         scanf("%d %d %d",&edge_rel[i].x,&edge_rel[i].y,&edge_rel[i].power);
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= num; i++)                                                   //初始化 顶点信息 和 邻接表
         {
-            one[i].group = 0;                                                         //初始化 顶点信息
+            one[i].group = 0;                                                        
             one[i].point = i;
+            list[i-1].firnode = i;
+            list[i-1].next = NULL;
         }
-    for(int i = 1;i <= edge;i++)
+    for(int i = 1;i <= edge;i++)                                                        //对每条边完成一次search（）
         search(edge_rel,one);
     printf("\n生成图的最小生成树的各边依次为：\n");                                      //输出结果
     for(int i = 0;i <= edge;i++)
         if(edge_rel[i].flag == 1) printf("(%d ,%d)\n",edge_rel[i].x,edge_rel[i].y);
+    printf("已储存最小生成树\n");
     return 0;
 }
-void search(relation *a,each_point *b)
+int search(relation *a,each_point *b)
 {
     int temp = -1,pos = -1;
     int i;
@@ -62,6 +79,7 @@ void search(relation *a,each_point *b)
                     }
             }
         }
+    if(pos == -1) return 0; 
     a[pos].flag = 1;
     if(b[a[pos].x].group == 0 && b[a[pos].y].group == 0)                                      //即，没有组别时，初始化一个组别
     {
@@ -73,4 +91,38 @@ void search(relation *a,each_point *b)
         if(b[a[pos].x].group != 0)     b[a[pos].y].group = b[a[pos].x].group;
         if(b[a[pos].y].group != 0)     b[a[pos].x].group = b[a[pos].y].group;
     }
+    save(a[pos]);
+    return 0;
+}
+void save(relation a)
+{
+    acrP px = list[a.x-1].next , py = list[a.y-1].next;
+    if(px == NULL)
+    {
+        list[a.x-1].next = (acrP) malloc(sizeof(acrNode));
+        px = list[a.x-1].next;
+    }
+    else
+    {
+        while(px->next != NULL) px = px->next;
+        px->next = (acrP) malloc(sizeof(acrNode));
+        px = px->next;
+    }
+    px->point = a.y;
+    px->power = a.power;
+    px->next = NULL;
+    if(py == NULL)
+    {
+        list[a.y-1].next = (acrP) malloc(sizeof(acrNode));
+        py = list[a.y-1].next;
+    }
+    else
+    {
+        while(py->next != NULL) py = py->next;
+        py->next = (acrP) malloc(sizeof(acrNode));
+        py = py->next;
+    }
+    py->point = a.x;
+    py->power = a.power;
+    py->next = NULL;
 }
